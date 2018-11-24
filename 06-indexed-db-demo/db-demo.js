b335d43cfb30bad3d6a4600e83fa6833e4958dac
Dilven <krystian.mateusiak@almservices.pl>
2018-11-24 13:58:08 +0100

    indexed-db

--- 06-indexed-db-demo/db-demo.js
+++ 06-indexed-db-demo/db-demo.js
@@ -1,6 +1,6 @@
 const db = new Dexie("demo06");
 db.version(1).stores({
-  users: "name,surname"
+  users: "name,surname,age"
 });
 
 db.users
@@ -12,11 +12,15 @@ db.users
 document.querySelector("#button").addEventListener("click", async () => {
   const nameInput = document.querySelector("#name");
   const surnameInput = document.querySelector("#surname");
+  const ageInput = document.querySelector("#age");
+
   const user = {
     name: nameInput.value,
-    surname: surnameInput.value
+    surname: surnameInput.value,
+    age: ageInput.value
   };
   await db.users.put(user);
   nameInput.value = "";
   surnameInput.value = "";
+  ageInput.value = "";
 });
